dd7c1c0822d76fe11c1c5e4cdda55f89892d4f1d
Michael Galetzka <galetzka.michael@gmail.com>
2016-06-28 15:08:58 +0200

    performance timing

--- main.cpp
+++ main.cpp
@@ -11,25 +11,28 @@ int main() {
     std::random_device rd;
     std::mt19937 eng(rd());
 
-    auto start = GetTimeMs64();
+
     vector<Boid> boids;
     boids.push_back(Boid(Vec3(1, 0.2, 0.2), Vec3(1, 0, 0)));
     boids.push_back(Boid(Vec3(1.5, 0, 0), Vec3(1, 0, 0)));
     //boids.push_back(Boid(Vec3(2, 2, 0), Vec3(1, 0, 0)));
     Swarm swarm(&boids);
     swarm.Update(1);
-    cout << "Time 3: " << (GetTimeMs64() - start) << endl;
 
     exit(0);
     std::uniform_real_distribution<float> dist(0, 20);
-    boids.clear();
-    for (int i = 0; i < 1000; i++) {
-        boids.push_back(Boid(Vec3(dist(eng), dist(eng), dist(eng)), Vec3::GetRandomUniform(eng)));
+    for (int k = 1; k < 100; k++) {
+        int size = k * 100;
+        boids.clear();
+        for (int i = 0; i < size; i++) {
+            boids.push_back(Boid(Vec3(dist(eng), dist(eng), dist(eng)), Vec3::GetRandomUniform(eng)));
+        }
+        auto start = GetTimeMs64();
+        Swarm swarm100(&boids);
+        swarm100.Update(1);
+        cout << size << ";" << (GetTimeMs64() - start) << "\n";
     }
-    start = GetTimeMs64();
-    Swarm swarm100(&boids);
-    swarm100.Update(1);
-    cout << "Time 100: " << (GetTimeMs64() - start) << endl;
+    cout << endl;
 
     return 0;
 }
